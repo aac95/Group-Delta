3f53794756b5bbc7e1479ec9c108ab1f10ea8358
Thomas Driscoll <tjd61@pitt.edu>
2021-03-19 11:20:01 -0400

    Clean up file and include line sensor main functions

--- JackiFSMmain.c
+++ JackiFSMmain.c
@@ -1,56 +1,36 @@
-//*****************************************************************************
-//
-// Jacki FSM test main
-// MSP432 with Jacki
-// Daniel and Jonathan Valvano
-// July 11, 2019
-/* This example accompanies the book
-   "Embedded Systems: Introduction to Robotics,
-   Jonathan W. Valvano, ISBN: 9781074544300, copyright (c) 2019
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
-
-Simplified BSD License (FreeBSD License)
-Copyright (c) 2019, Jonathan Valvano, All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice,
-   this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-The views and conclusions contained in the software and documentation are
-those of the authors and should not be interpreted as representing official
-policies, either expressed or implied, of the FreeBSD Project.
-*/
+/*
+ * Team Delta Design Project 1
+ * Authors. Anthony Caamano, Haleigh Defoor, Preston Brazzle, Thomas Driscoll
+ * ECE 1188, Cyber-Pysical Systems
+ * University of Pittsburgh
+ * March 2021
+ *
+ * This code implements a line-following state machine that incorporates
+ *  - Bump sensor interrupt
+ *  - Line sensor SysTick interrupt
+ *  - PWM motor control
+ *  - Data logging
+ */
 
 #include <stdint.h>
 #include "msp.h"
+#include "../inc/AP.h"
+#include "../inc/BumpInt.h"
 #include "../inc/Clock.h"
 #include "../inc/CortexM.h"
-#include "../inc/PWM.h"
+#include "../inc/FlashProgram.h"
 #include "../inc/LaunchPad.h"
-#include "../inc/TExaS.h"
-#include "../inc/AP.h"
-#include "../inc/UART0.h"
-#include "../inc/BumpInt.h"
-#include "../inc/Reflectance.h"
 #include "../inc/Motor.h"
+#include "../inc/PWM.h"
+#include "../inc/Reflectance.h"
+#include "../inc/SysTickInts.h"
+#include "../inc/TExaS.h"
+#include "../inc/UART0.h"
 
+
+/*********************************
+ *      STATE MACHINE
+ *********************************/
 // Linked data structure
 struct State {
   uint32_t out;                // 2-bit output
@@ -85,13 +65,41 @@ State_t *Spt;  // pointer to the current state
 uint32_t Input;
 uint32_t Output;
 
+/*********************************
+ *      BUMP SENSOR INTERRUPT
+ *********************************/
+
 void HandleCollision(uint8_t bumpSensor){
    Motor_Stop();
 }
 
+/*********************************
+ *      LINE SENSOR INTERRUPT
+ *********************************/
+
+uint8_t reading;
+
+void SysTick_Handler(void){ // every 1ms
+  // write this as part of Lab 10
+    uint32_t i=0;
+    while(1){
+        i++;
+        if (i%10==0)
+            Reflectance_Start();
+        else if (i%10==1)
+            reading = Reflectance_End();
+    }
+}
+
+/*********************************
+ *      MAIN FUNCTION
+ *********************************/
+
 void main(void){
     Clock_Init48MHz();
     BumpInt_Init(&HandleCollision);
+    Reflectance_Init();
+    SysTick_Init(48000,2);
     LaunchPad_Init();
 
     Spt = Center;
