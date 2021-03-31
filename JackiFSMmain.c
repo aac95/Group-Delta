41148bf6900d9807639b8349cd68838af0ef1892
Preston Brazzle <71470122+pbrazzle@users.noreply.github.com>
2021-03-31 14:36:22 -0400

    Add files via upload

--- JackiFSMmain.c
+++ JackiFSMmain.c
@@ -27,7 +27,7 @@
 #include "../inc/SysTickInts.h"
 #include "../inc/TExaS.h"
 #include "../inc/UART0.h"
-#include "Debug.c"
+//#include "Debug.c"
 
 /*********************************
  *      STATE MACHINE
@@ -45,37 +45,39 @@ typedef const struct State State_t;
 #define Left1   &fsm[1]
 #define Left2   &fsm[2]
 #define OffL1   &fsm[3] //5000
-#define Off2    &fsm[4] //5000
+#define OffL2    &fsm[4] //5000
 #define Stop    &fsm[5]
 #define OffR1   &fsm[6] //5000
-#define Right1  &fsm[7]
-#define Right2  &fsm[8]
-#define FarR    &fsm[9] //1000
-#define AngledR &fsm[10] //3000
-#define AngledL &fsm[11] //3000
-#define FarL    &fsm[12] //1000
+#define OffR2   &fsm[7]
+#define Right1  &fsm[8]
+#define Right2  &fsm[9]
+#define FarR    &fsm[10] //1000
+#define AngledR &fsm[11] //3000
+#define AngledL &fsm[12] //3000
+#define FarL    &fsm[13] //1000
 
-State_t fsm[13]={
+State_t fsm[14]={
                            //0000,  0001,   0010,   0011,   0100,   0101,   0110,   0111,    1000, 1001, 1010,  1011,   1100,  1101,    1110,    1111
-{PERIOD,    PERIOD,    10,  {Off2, FarR,   Right1, Right1, Left1,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}},  //Center
-{PERIOD/4,  PERIOD/2,  20,  {OffL1, FarR,   Right1, Right1, Left2,  Left2,  Center, AngledR, FarL, FarL, Left2, Right1, Left2, AngledL, AngledL, Center}},  //Left1
-{PERIOD,    PERIOD,    10,  {OffL1, FarR,   Right1, Right1, Left1,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}},  //Left2
-{PERIOD,    -PERIOD,   350, {Off2,  Off2,   Off2,   Off2,   Off2,   Off2,   Off2,   Off2,    Off2, Off2, Off2,  Off2,   Off2,  Off2,    Off2,    Center}},  //OffL1
-{PERIOD,    PERIOD,    100, {Stop,  FarR,   Right1, Right1, Left1,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}},  //Off2
-{0,         0,         10,  {Stop,  FarR,   Right1, Right1, Left1,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}},  //Stop
-{-PERIOD,    PERIOD,   350, {Off2,  Off2,   Off2,   Off2,   Off2,   Off2,   Off2,   Off2,    Off2, Off2, Off2,  Off2,   Off2,  Off2,    Off2,    Center}},  //OffR1
-{PERIOD/2,  PERIOD/4,  20,  {OffR1, FarR,   Right2, Right2, Left1,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}},  //Right1
-{PERIOD,    PERIOD,    10,  {OffR1, FarR,   Right1, Right1, Left1,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}},  //Right2
-{PERIOD,    0,         10,  {OffR1, Right2, Right1, Right1, Left1,  Left1,  Center, Center,  FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}},  //FarR
-{PERIOD,    PERIOD/3,  10,  {OffR1, FarR,   Right1, Right1, Left1,  Left1,  Center, Right2,  FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}},  //AngledR
-{PERIOD/3,  PERIOD,    10,  {OffL1, FarR,   Right1, Right1, Left2,  Left2,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, Left2,   Left2,   Center}},  //AngledL
-{0,         PERIOD,    10,  {OffL1, FarR,   Right1, Right1, Left1,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, Center,  Center}}   //FarL
+{PERIOD,    PERIOD,    10,  {OffL2,  FarR,  AngledR, Right1, AngledL,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}}, //Center
+{PERIOD/4,  PERIOD/2,  10,  {OffL1, FarR,   AngledR, Right1, AngledL,  Left2,  Center, AngledR, FarL, FarL, Left2, Right1, Left2, AngledL, AngledL, Center}}, //Left1
+{PERIOD,    PERIOD,    10,  {OffL1, FarR,   AngledR, Right1, AngledL,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}}, //Left2
+{PERIOD,    -PERIOD,   20,  {OffL2, FarR,   AngledR, Right1, AngledL,  OffL2,   Center, AngledR, FarL, OffL2, OffL2,  OffL2,   Left1, OffL2,    AngledL, Center}}, //OffL1
+{PERIOD,    PERIOD,    10,  {OffL1, FarR,   AngledR, Right1, AngledL,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}}, //OffL2
+{0,         0,         10,  {Stop,  FarR,   AngledR, Right1, AngledL,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}}, //Stop
+{-PERIOD,    PERIOD,   20,  {OffR2, FarR,   AngledR, Right1, AngledL,  OffR2,   Center, AngledR, FarL, OffR2, OffR2,  OffR2,   Left1, OffR2,    AngledL, Center}}, //OffR1
+{-PERIOD,    PERIOD,   10,  {OffR1, FarR,   AngledR, Right1, AngledL,  OffR2,   Center, AngledR, FarL, OffR2, OffR2,  OffR2,   Left1, OffR2,    AngledL, Center}}, //OffR2
+{PERIOD/2,  PERIOD/4,  10,  {OffR1, FarR,   AngledR, Right2, AngledL,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}}, //Right1
+{PERIOD,    PERIOD,    10,  {OffR1, FarR,   AngledR, Right1, AngledL,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}}, //Right2
+{PERIOD,    0,         10,  {OffR1, Right2, AngledR, Right1, AngledL,  Left1,  Center, Center,  FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}}, //FarR
+{PERIOD,    PERIOD/2,  10,  {OffR1, FarR,   AngledR, Right1, AngledL,  Left1,  Center, Right2,  FarL, FarL, Left1, Right1, Left1, AngledL, AngledL, Center}}, //AngledR
+{PERIOD/2,  PERIOD,    10,  {OffL1, FarR,   AngledR, Right1, AngledL,  Left2,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, Left2,   Left2,   Center}}, //AngledL
+{0,         PERIOD,    10,  {OffL1, FarR,   AngledR, Right1, AngledL,  Left1,  Center, AngledR, FarL, FarL, Left1, Right1, Left1, AngledL, Center,  Center}}  //FarL
 };
 
 State_t *Spt;  // pointer to the current state
 uint32_t Input;
 uint32_t Output;
-float globalSpeed = 0.30;
+float globalSpeed = 0.5;
 
 /*********************************
  *      BUMP SENSOR INTERRUPT
@@ -113,27 +115,27 @@ void SysTick_Handler(void){ // every 1ms
 #define leftSoftMask    0b00100000
 #define centerMask      0b00111100
 
-#define simpleMask1 0b10000000
+#define simpleMask1 0b11000000
 #define simpleMask2 0b00110000
 #define simpleMask3 0b00001100
-#define simpleMask4 0b00000001
+#define simpleMask4 0b00000011
 
 uint8_t buffer[256*2];
 uint8_t adjustReadingTo4(uint8_t readValue) {
-    /*
     uint8_t r = 0;
     if (readValue & simpleMask1) r += 0b1000;
     if (readValue & simpleMask2) r += 0b0100;
     if (readValue & simpleMask3) r += 0b0010;
     if (readValue & simpleMask4) r += 0b0001;
     return r;
-    */
 
+    /*
     uint8_t r = 0;
     if (centerMask & readValue) r += 0b0110;
     if (rightHardMask & readValue) r += 0b0001;
     if (leftHardMask & readValue) r += 0b1000;
     return r;
+    */
 
     /*
     if(!readValue)                                                  return 0b0000;
@@ -173,17 +175,20 @@ void main(void){
         Motor_DutyLeft(Spt->left * globalSpeed);      //Drive Left Motor
         Motor_DutyRight(Spt->right * globalSpeed);    //Drive Right Motor
         Clock_Delay1ms(Spt->delay);     // wait
+        Reflectance_Start();
+        Clock_Delay1ms(1);
+        reading = Reflectance_End();
         adjusted = adjustReadingTo4(reading);
-        // Code to make ROM Debug work
-        buffer[bindex]= adjusted; // adds adjusted to buffer
+        /*
+        buffer[bindex]= adjusted;
         if(bindex<256*2)
-            bindex++; // increments index
+            bindex++;
         else
         {
-            bindex=0; // resets index when buffer is full
-            Debug_FlashRecord((uint16_t *) buffer); // puts bufer into ROM
+            bindex=0;
+            Debug_FlashRecord((uint16_t *) buffer);
         }
-        // End of code to make ROM debug work
+        */
         Spt = Spt->next[adjusted];       // next depends on input and state
     }
 
